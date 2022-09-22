f65eaacd6beaf7203ce3532c294e781641dca29a
Preeti Patil <107602459+Preeti1810@users.noreply.github.com>
2022-09-22 15:05:37 +0530

    Add files via upload

---
+++ MV.R
@@ -0,0 +1,15 @@
+#measuing variabilties
+
+Q7
+var(Q7$Points)
+var(Q7$Score)
+var(Q7$Weigh)
+
+Std_deviation1 = sqrt(var(Q7$Points));Std_deviation1
+Std_deviation2 = sqrt(var(Q7$Score));Std_deviation2
+Std_deviation3 = sqrt(var(Q7$Weigh));Std_deviation3
+
+range(Q7$Points,na.rm = TRUE)
+range(Q7$Score,na.rm = TRUE)
+range(Q7$Weigh,na.rm = TRUE)
+
